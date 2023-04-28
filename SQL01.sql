dc9f0644649b1b9db35537f4e9b177556025ac0e
thitipun7024 <thitipun7024@gmail.com>
2023-04-28 15:01:45 +0700

    edit

---
+++ SQL01.sql
@@ -0,0 +1,33 @@
+SELECT TOP (1000000) [id]
+      ,[brn]
+      ,[bnname]
+      ,[address]
+      ,[postcode]
+      ,[mobile]
+      ,[internal]
+      ,[Latitude]
+      ,[Longitude]
+      ,[region]
+      ,[district]
+      ,[headbranch]
+      ,[area]
+  FROM [IT].[dbo].[MasterControlAddress] 
+
+SELECT id,brn,bnname,mobile,region,internal
+FROM MasterControlAddress
+WHERE region 
+IN (N'ภาคธุรกิจที่ 3',N'ภาคธุรกิจที่ 4') AND internal IS NULL
+
+SELECT * FROM MasterControlAddress
+
+SELECT MasterControlAddress.region,count(MasterControlAddress.region) AS regiontotal
+FROM MasterControlAddress
+WHERE region 
+IN  (N'ภาคธุรกิจที่ 3',N'ภาคธุรกิจที่ 4') GROUP BY region
+
+SELECT count(MasterControlAddress.region) AS regiontotal
+FROM MasterControlAddress
+WHERE region 
+IN  (N'ภาคธุรกิจที่ 3',N'ภาคธุรกิจที่ 4') GROUP BY region
+
+SELECT FROM INNER JOIN WHERE 
