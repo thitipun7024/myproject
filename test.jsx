09a92dc8726c35cfb2f8f4cadce12e5894e5baa7
thitipun7024 <130922221+thitipun7024@users.noreply.github.com>
2023-05-08 14:26:49 +0700

    edit

---
+++ test.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+function LoginForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // TODO: handle form submission
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={(event) => setUsername(event.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+      </label>
+      <button type="submit">Login</button>
+    </form>
+  );
+}
